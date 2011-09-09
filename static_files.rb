8b47ec59fe215198e7fd70548d8792b19df9392c
Tom Winkler <thomas.winkler@starseeders.net>
2011-09-09 23:37:31 +0200

    fix: static file handling/unicorn restart

--- static_files.rb
+++ static_files.rb
@@ -1,7 +1,7 @@
 Capistrano::Configuration.instance.load do
   task :after_update_code do
     %w(tmp/pids tmp/sockets public/assets public/system).each do |share|
-      run "if [ ! -d #{shared_path}/#{share} ]; then mkdir #{shared_path}/#{share} ; fi"
+      run "if [ ! -d #{shared_path}/#{share} ]; then mkdir -p #{shared_path}/#{share} ; fi"
       run "ln -s -f #{shared_path}/#{share} #{release_path}/#{share}"
     end
   end
