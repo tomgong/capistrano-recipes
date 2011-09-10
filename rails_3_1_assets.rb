56f1f9a2c083738b25320ad28e3c5571e00e32c8
Andreas Happe <andreashappe@snikt.net>
2011-09-10 03:44:33 +0200

    fix: thinko

--- rails_3_1_assets.rb
+++ rails_3_1_assets.rb
@@ -1,6 +1,8 @@
-before :"deploy:symlink", :"deploy:assets";
+Capistrano::Configuration.instance.load do
+  before :"deploy:symlink", :"deploy:assets";
 
-desc "Compile asets"
-task :assets do
-  run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
+  desc "Compile asets"
+  task :assets do
+    run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
+  end
 end
