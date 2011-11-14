88b61f87236eee464df7ad75ce7fc6e2ca60d537
Andreas Happe <andreashappe@snikt.net>
2011-11-14 18:59:48 +0100

    remove rails 3.1 support (is now supported by capistrano itself)

--- static_files.rb
+++ static_files.rb
@@ -1,6 +1,6 @@
 Capistrano::Configuration.instance.load do
   task :after_update_code do
-    %w(tmp/pids tmp/sockets public/assets public/system log).each do |share|
+    %w(tmp/pids tmp/sockets public/assets public/system log public/uploads).each do |share|
       run "if [ ! -d #{shared_path}/#{share} ]; then mkdir -p #{shared_path}/#{share} ; fi"
       run "ln -s -f #{shared_path}/#{share} #{release_path}/#{share}"
     end
