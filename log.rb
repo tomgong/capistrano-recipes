42dead7bb61068e4b035d25c5815f734a26dbab7
Thomas Winkler <tommy.winkler@gmail.com>
2012-01-06 16:19:20 +0100

    add log tasks

---
+++ log.rb
@@ -0,0 +1,31 @@
+Capistrano::Configuration.instance.load do
+  namespace :log do
+    desc "Tail all application log files"
+    task :tail do
+      run "tail -f #{shared_path}/log/*.log" do |channel, stream, data|
+        puts "#{channel[:host]}: #{data}"
+        break if stream == :err
+      end
+    end
+
+    desc <<-DESC
+Install log rotation script; optional args: days=7, size=5M, group (defaults to same value as :user)
+    DESC
+
+    task :rotate do
+      rotate_script = %Q{#{shared_path}/log/#{environment}.log {
+daily
+rotate #{ENV['days'] || 7}
+size #{ENV['size'] || "5M"}
+compress
+create 640 #{user} #{ENV['group'] || user}
+missingok
+}}
+      put rotate_script, "#{shared_path}/logrotate_script"
+      "#{sudo} cp #{shared_path}/logrotate_script /etc/logrotate.d/rails_#{application}"
+      run "rm #{shared_path}/logrotate_script"
+    end
+  end
+
+  on :before, "log:rotate", :only => 'deploy:start'
+end
