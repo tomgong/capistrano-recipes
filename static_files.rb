5edfbfbc966532740ee65b7ac6b208c2450aff0f
Andreas Happe <andreashappe@snikt.net>
2012-02-29 23:10:59 +0100

    fix the latest capistrano fallotu

--- static_files.rb
+++ static_files.rb
@@ -1,9 +1,7 @@
 Capistrano::Configuration.instance.load do
-  after "deploy:update_code" do
-    raise "test"
-
+  after "deploy:assets:symlink" do
+  #after "deploy:update_code" do
     %w(tmp/pids tmp/sockets public/assets public/system log public/uploads).each do |share|
-      raise "fut"
       run "if [ ! -d #{shared_path}/#{share} ]; then mkdir -p #{shared_path}/#{share} ; fi"
       run "ln -s -f #{shared_path}/#{share} #{release_path}/#{share}"
     end
