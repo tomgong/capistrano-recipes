b87a1433497578269630826da4347f2fa222f188
Andreas Happe <andreashappe@snikt.net>
2012-04-03 18:14:15 +0200

    further refinements

--- db_backup.rb
+++
@@ -1,8 +0,0 @@
-# backup postgres database into a database-agnostic file
-namespace :backup do
-  desc "Dumps target database db into an file"
-  task :database do
-    run "cd #{current_release} && rvmsudo RAILS_ENV=\"production\" rake db:dump"
-  end
-  before 'deploy:migrate', 'backup:database'
-end
--- postgres.rb
+++ postgres.rb
@@ -23,6 +23,28 @@ namespace :postgres do
       puts "cannot configure the database as it isn't localhost or driver ain't postgresql"
       exit
     end
+
+    run "#{sudo} mkdir -p #{shared_path}/db_backups" 
+    run "#{sudo} chown postgres:postgres #{shared_path}/db_backups"
   end
   after "deploy:setup", "postgres:create_database"
+
+  desc "Dumps target database db into an file"
+  task :database do
+    # read database config
+    config = YAML.load_file("config/database.yml")
+    adapter = config[stage.to_s]["adapter"]
+    hostname = config[stage.to_s]["host"]
+    username = config[stage.to_s]["username"]
+    password = config[stage.to_s]["password"]
+    database = config[stage.to_s]["database"]
+    
+    if adapter == "postgresql" && hostname == "localhost"
+      run %Q{#{sudo} -u postgres pg_dump #{database} -f #{shared_path}/db_backups/dump-#{Time.now.strftime("%Y%m%d-%H%M")}.sql;"}
+    else
+      puts "cannot dump the database as it isn't localhost or driver ain't postgresql"
+      exit
+    end
+  end
+  before 'deploy:migrate', 'backup:database'
 end
