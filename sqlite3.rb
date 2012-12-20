84f381e3f724e261e2391c106571ff80747010e3
Thomas Winkler <thomas.winkler@starseeders.net>
2012-12-20 09:02:00 +0100

    add sqlite3 recipe for sqlite db in production

---
+++ sqlite3.rb
@@ -0,0 +1,17 @@
+set(:shared_database_path) {"#{shared_path}/db"}
+
+namespace :sqlite3 do
+  desc "Symlink for production.sqlite3 db"
+  task :symlink_db do
+    run "ln -nfs #{shared_database_path}/production.sqlite3 #{current_release}/db/production.sqlite3"
+  end
+ 
+  desc "Make a shared database folder"
+  task :make_shared_folder, :roles => :db do
+    run "mkdir -p #{shared_database_path}"
+  end
+  
+  after "deploy:setup", "sqlite3:make_shared_folder"
+  after "deploy:update_code", "sqlite3:symlink_db"
+  before "deploy:migrate", "sqlite3:symlink_db"
+end
