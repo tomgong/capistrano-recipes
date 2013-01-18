4fa6079466713055acd7bb3fe91a7bde0ec0d675
Tom Winkler <thomas.winkler@starseeders.net>
2013-01-18 10:24:30 +0100

    fixed asset precompilation on first deployment

--- lib/recipes/assets.rb
+++ lib/recipes/assets.rb
@@ -7,11 +7,17 @@ Capistrano::Configuration.instance(:must_exist).load do
     namespace :assets do
       task :precompile, :roles => :web, :except => { :no_release => true } do
         logger.info "Checking asset dirs for precompilation: #{asset_locations.join(" ")} #{additional_asset_locations.join(" ")}"
-        from = source.next_revision(current_revision)
-        if capture("cd #{latest_release} && #{source.local.log(from)} #{asset_locations.join(" ")} #{additional_asset_locations.join(" ")} | wc -l").to_i > 0
+        first_deplopyment = false
+        begin
+          from = source.next_revision(current_revision)
+        rescue
+          first_deplopyment = true
+        end
+        
+        if !first_deployment || capture("cd #{latest_release} && #{source.local.log(from)} #{asset_locations.join(" ")} #{additional_asset_locations.join(" ")} | wc -l").to_i > 0
           run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
         else
-          logger.info "Skipping asset pre-compilation because there were no asset changes"
+          logger.info "Skipping asset pre-compilation because there were no asset changes or its the first deployment"
         end
       end
     end
